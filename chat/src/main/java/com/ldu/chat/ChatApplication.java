a808731e61cc3aa6fed07ca7ee791d76da6ccf0f
ldu <lduk3043@hkcloud.co.kr>
2022-12-09 12:24:04 +0900

    jpa, db(oracle) 연동

--- chat/src/main/java/com/ldu/chat/ChatApplication.java
+++ chat/src/main/java/com/ldu/chat/ChatApplication.java
@@ -2,12 +2,25 @@ package com.ldu.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+import com.ldu.chat.jpa.user.entity.UserEntity;
+import com.ldu.chat.jpa.user.repository.UserRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ChatApplication {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
+//		SpringApplication.run(ChatApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ChatApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+
+		UserEntity user = userRepository.findByUserId("test");
+		
+		log.debug("test ::::::::::::: {}" , user);
 	}
 
 }
---
+++ chat/src/main/java/com/ldu/chat/jpa/user/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.ldu.chat.jpa.user.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="TB_USER", schema="CHAT")
+public class UserEntity implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String userId;			// 사용자id
+	private String userName;		// 사용자명
+	private String userPassword;	// 사용자비밀번호
+	
+	@CreationTimestamp
+	private LocalDateTime regDate;	// 등록일시
+	@UpdateTimestamp
+	private LocalDateTime updDate;	// 수정일시
+}
